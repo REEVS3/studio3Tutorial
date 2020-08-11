543ee7dac4ec9925e2bc7cbfa72bf0c6e0fef5d2
Seymour <reevs3@student.op.ac.nz>
2020-08-12 11:56:49 +1200

    Added All pages etc

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center mt-5">
+    <div class="inner">
+        <p>&copy; <a href="https://github.com/REEVS3/studio3Tutorial" target="_blank">Seymour Reeves-Boddy</a> 2020</p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>@yield('title','Laravel 5.8 Basics')</title>
+</head>
+<body>
+
+    @include('inc.navbar')
+
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    @include('inc.footer')
+
+    <script src="{{asset('js/app.js')}}"></script> 
+
+</body>
+</html>
